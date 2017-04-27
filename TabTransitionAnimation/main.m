070b3c1a7cdee20c65b14128bad447ff19eba0f2
oudushu <516661710@qq.com>
2017-04-27 11:04:21 +0800

    Initial Commit

---
+++ TabTransitionAnimation/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TabTransitionAnimation
+//
+//  Created by 欧杜书 on 27/04/2017.
+//  Copyright © 2017 欧杜书. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
